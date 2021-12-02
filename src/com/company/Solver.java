da685a1f44dc7821a2cdd7f8b2c5f1ee70fd2c2c
M-Kia <smhkh2001@gmail.com>
2021-12-02 23:19:03 +0330

    .

--- src/com/company/Solver.java
+++ src/com/company/Solver.java
@@ -18,28 +18,44 @@ public class Solver {
         queue.add(new Node(new Puzzle(p)));
     }
 
+    public List<Node> filterChildren(List<Node> children) {
+        List<Node> temp = new ArrayList<>(children);
+        List<Node> t = new ArrayList<>(queue);
+        for (Node node : children) {
+            for (Node n : t) {
+                if (Arrays.equals(node.puzzle.state, n.puzzle.state)) {
+                    try {
+                        temp.remove(node);
+                    } catch (Exception ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                }
+            }
+        }
+        return temp;
+    }
+
     // حل کردن معما که در نهایت حالت نهایی را برمیگرداند.
-    public Node solve() {
+    public void solve() {
         while (queue.get(0).cost != 0) {
-                Node temp = queue.remove(0);
-                history.add(temp);
-                queue.addAll(temp.explore());
-                queue.sort((n1, n2) -> {
-                    if (n1.cost == 0) return -1;
-                    if (n2.cost == 0) return 1;
-                    int x1 = n1.cost + n1.depth, x2 = n2.cost + n2.depth;
-                    // مرتب کردن بر اساس تابع f
-                    if (x1 > x2) return 1;
-                    if (x1 < x2) return -1;
-                    // در صورت برابر بودن f، بر اساس هزینه گره مرتب می شوند.
-                    if (n1.cost > n2.cost) return 1;
-                    if (n1.cost < n2.cost) return -1;
-                    // اگر هنوز هم برابر بودند، بر اساس عمق مرتب میکند و همچنین برابری را چک میکند.
-                    return Integer.compare(n1.depth, n2.depth);
-                });
+            Node temp = queue.remove(0);
+            history.add(temp);
+            queue.addAll(filterChildren(temp.explore()));
+            queue.sort((n1, n2) -> {
+                if (n1.cost == 0) return -1;
+                if (n2.cost == 0) return 1;
+                int x1 = n1.cost + n1.depth, x2 = n2.cost + n2.depth;
+                // مرتب کردن بر اساس تابع f
+                if (x1 > x2) return 1;
+                if (x1 < x2) return -1;
+                // در صورت برابر بودن f، بر اساس هزینه گره مرتب می شوند.
+                if (n1.cost > n2.cost) return 1;
+                if (n1.cost < n2.cost) return -1;
+                // اگر هنوز هم برابر بودند، بر اساس عمق مرتب میکند و همچنین برابری را چک میکند.
+                return Integer.compare(n1.depth, n2.depth);
+            });
         }
         history.add(queue.get(0));
-        return queue.get(0);
     }
 
     @Override
@@ -47,7 +63,7 @@ public class Solver {
         String arrayFormat = "\t\t%d\t%d\t%d\n\t\t%d\t%d\t%d\n\t\t%d\t%d\t%d\n\n";
         Node node = queue.get(0);
         int[] state = node.puzzle.state;
-        String res =String.format(arrayFormat, state[0], state[1], state[2], state[3], state[4], state[5], state[6],
+        String res = String.format(arrayFormat, state[0], state[1], state[2], state[3], state[4], state[5], state[6],
                 state[7], state[8]);
         while (node.parentNode != null) {
             node = node.parentNode;
